06cffc2c2e703cba92336a368ddf719ba3d6c1f0
Victoria Blackburn <v.a.c.blackburn@gmail.com>
2021-05-14 12:39:44 +0100

    Add custom ExpressError that extends Error class

--- app.js
+++ app.js
@@ -39,10 +39,14 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new')
 })
 
-app.post('/campgrounds', async(req,res) => {
+app.post('/campgrounds', async(req, res, next) => {
+    try {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
+    } catch(e) {
+        next(e)
+    }
 })
 
 app.get('/campgrounds/:id', async(req, res) => {
@@ -66,6 +70,10 @@ app.delete('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+})
+
+app.use((err, req, res, next) => {
+    res.send('Uh oh, error!')
 })
 
 app.listen(3000, ()=> {
